1a07a210d24715da94bdebba0f414e95d8f21b5c
Ariel Macias <materchico@gmail.com>
2025-04-24 09:18:12 -0500

    Atributos

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,3 +1,23 @@
 public class Vehiculo {
+    // Atributos
+    String modelo;
+    String color;
+    double kilometraje;
+    String placa;
+
+    // Constructor
+    public Vehiculo(String modelo, String color, double kilometraje, String placa){
+        this.modelo = modelo;
+        this.color = color;
+        this.kilometraje = kilometraje;
+        this.placa = placa;
+    }
+
+
+
+
+
+
+
 
 }
