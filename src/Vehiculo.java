ce3cb5304f194394a51f30bf61f989aca6506673
Ardanny <ardannyromero@gmail.com>
2025-04-24 09:19:01 -0500

    metodo

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,3 +1,6 @@
 public class Vehiculo {
 
+    public void info_de_vehiculo   () {
+        System.out.printf("Vehiculo de modelo %s, color %s, placa %s \nTiene un kilometraje de: %.2f km",modelo,color,placa,kilometraje);
+    }
 }
